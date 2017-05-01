5ee5867f39b0a5f9702e1c68159045c8ecf61f2a
Administrator <Administrator@192.168.1.5>
2017-05-01 15:08:25 -0300

    primer

---
+++ src/tc3/tp2/genericas/OrtFileUtils.java
@@ -0,0 +1,23 @@
+package tc3.tp2.genericas;
+
+import java.io.IOException;
+import java.io.File;
+
+public class OrtFileUtils {
+
+	public static String getFullFileName(String filename) throws IOException {
+		
+		return " ";
+	}
+
+	public static String getCanonicalPath() throws IOException {
+
+		return "";
+	}
+
+	public static String checkFolder(String pathname, boolean crear) throws RuntimeException, IOException {
+		
+		return "";
+	}
+
+}
